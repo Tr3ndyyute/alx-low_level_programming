b047253dc2d907b1c70f287e9c7e838129a8bdac
Tr3ndyyute <101469816+Tr3ndyyute@users.noreply.github.com>
2023-01-21 14:19:17 +0100

    Create div.c

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
